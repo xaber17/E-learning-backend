d53c8ac1f257e6a61fa7e0f57c62f8a6cd57fd65
Getarnr <getnuansaa@gmail.com>
2023-06-22 18:49:22 +0700

    feat: import module

--- src/jawaban/jawaban.module.ts
+++ src/jawaban/jawaban.module.ts
@@ -4,9 +4,12 @@ import { JawabanController } from './jawaban.controller';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SoalsEntity } from 'src/soal/entities/soal.entity';
+import { KelassEntity } from 'src/kelas/entities/kelas.entity';
+import { Jawaban } from './entities/jawaban.entity';
+import { UsersEntity } from 'src/user/entities/users.entity';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([SoalsEntity])],
+  imports: [ConfigModule, TypeOrmModule.forFeature([SoalsEntity, KelassEntity, Jawaban, UsersEntity])],
   controllers: [JawabanController],
   providers: [JawabanService],
   exports: [JawabanService],
